96e7f0bb85eee82ae11ce371c1182b834c91fcb5
Josefina <jose.balmacedal@gmail.com>
2016-03-17 17:56:13 -0300

    bg home, footer and seed user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user1 = User.find_by_email('franco@nnodes.com')
+user1 = User.create(email: 'franco@nnodes.com', password: '123123123') if !user1.present?
+
+user2 = User.find_by_email('josefina@nnodes.com')
+user2 = User.create(email: 'josefina@nnodes.com', password: '123123123') if !user2.present?
+
+user3 = User.find_by_email('natalia@nnodes.com')
+user3 = User.create(email: 'natalia@nnodes.com', password: '123123123') if !user3.present?
+
+user4 = User.find_by_email('pablo@nnodes.com')
+user4 = User.create(email: 'pablo@nnodes.com', password: '123123123') if !user3.present?
+
+user5 = User.find_by_email('eduardo@nnodes.com')
+user5 = User.create(email: 'eduardo@nnodes.com', password: '123123123') if !user3.present?
